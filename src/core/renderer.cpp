f9ddd8ccac46da5f91d927925e328dd7d76b28ec
XReaper95 <luis.ernesto951008@gmail.com>
2025-04-19 19:37:51 -0300

    Add a global menu for Dev UI

--- src/core/renderer.cpp
+++ src/core/renderer.cpp
@@ -3,13 +3,35 @@
 #include <dev_ui/dev_ui.h>
 #include <glad/gl.h>
 
-using namespace dev_ui;
-
 void core::Renderer::Render() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void core::Renderer::RenderDevUi() {
-    im::ShowDemoWindow(nullptr);
+    using namespace dev_ui;
+    
+    im::Begin("Learning OpenGL");
+
+    if (ImGui::CollapsingHeader("Global Shortcuts"))
+    {
+        static constexpr ImGuiTableFlags tableFlags =
+            ImGuiTableFlags_RowBg | ImGuiTableFlags_Borders;
+        static constexpr auto yellow = ImVec4(1.0f, 1.0f, 0.0f, 1.0f);
+        if (im::BeginTable("shortcuts", 2, tableFlags))
+        {
+            im::TableSetupColumn("Shortcut", ImGuiTableColumnFlags_WidthFixed);
+            im::TableSetupColumn("Description", ImGuiTableColumnFlags_WidthFixed);
+            im::TableHeadersRow();
+            im::TableNextRow();
+            im::TableNextColumn();
+            im::Indent();
+            im::TextColored(yellow, "U");
+            im::TableNextColumn();
+            im::Text("Enables wireframe mode");
+            im::EndTable();
+        }
+    }
+
+    im::End();
 }
